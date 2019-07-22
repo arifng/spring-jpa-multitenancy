c8fb619f00e722c0606a908f5895ede202d434f3
Rana <arifng@gmail.com>
2019-07-22 11:35:41 +0600

    refactor

--- src/main/java/com/arifng/dbmultitenancy/webconfig/WebMvcConfig.java
+++ src/main/java/com/arifng/dbmultitenancy/webconfig/WebMvcConfig.java
@@ -1,12 +1,14 @@
 package com.arifng.dbmultitenancy.webconfig;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Created by rana on 22/7/19.
  */
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
